5ab311af142ba3b0b6fd9eef4ff98deda2ad7b14
OsirisLambert <35780056+OsirisLambert@users.noreply.github.com>
2020-05-03 17:18:14 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -31,9 +31,32 @@ def new_post(_community, post):
     r = requests.post(url, json=data, headers=headers)
     return r
 
-def create_feed(http_id, ):
+def create_25_posts_to_any_community():
+    resp = view_all(5)
+    assert resp.status_code == 200, 'FAIL - Expected status code 200. Got status code' + str(resp.status_code)
+    data = json.loads(resp.json()['data'])
     fg = FeedGenerator()
-    fg.id(http_id)
+    fg.id('http://localhost:2015/posts/all/recent/25')
+    fg.title('Microservice - view all')
+    fg.link( href='http://localhost:2015/posts/all/recent/25', rel='alternate' )
+    fg.description('The 25 most recent posts to any community')
+    fg.language('en')
+
+    for i in data:
+        print(type(i))
+        fe = fg.add_entry()
+        fe.id('http://localhost:2015')
+        fe.author(name = i['author'])
+        fe.title(i['title'])
+        fe.description(i['community'])
+        fe.summary(i['date'])
+        #fe.content(content=i['text'])
+
+
+
+    rssfeed  = fg.rss_str(pretty=True) # Get the RSS feed as string
+    fg.rss_file('AllPosts.xml') # Write the RSS feed to a file
+    fg.rss_file('AllPosts.rss') # Write the RSS feed to a file
 
 def main():
     parser = argparse.ArgumentParser(description='CPSC 449 project 2')
@@ -84,15 +107,8 @@ def main():
         LIST N MOST RECENT POST FOR ALL COMMUNITIES
     """
     print( "\n3 - List the n most recent posts to any community\n")
-    print( " n = 3 ")
-    resp = view_all(3)
-    assert resp.status_code == 200, 'FAIL - Expected status code 200. Got status code' + str(resp.status_code)
-    print(len(resp.json()['data']))
-    #assert len(resp.json()['data']) == 3 , "FAIL - Expected the length of the json to be 3. Instead received" + str(len(resp.json()["data"]))
-    print("PASS - view all last 3 posts returned 3 posts")
-    if(args.verbose):
-        print(resp.json()['data'])
-
+    create_25_posts_to_any_community()
+    """
     print( " n = 5 ")
     resp = view_all(5)
     assert resp.status_code == 200, 'FAIL - Expected status code 200. Got status code' + str(resp.status_code)
@@ -101,19 +117,11 @@ def main():
     if(args.verbose):
         print(resp.json()['data'])
 
-    """
+
         LIST N MOST RECENT POST FOR A PARTICULAR COMMUNITY
-    """
+
     print( "\n4 - List the n most recent posts to a particular community\n")
     print( " In this case the particular community is CSUF-CPSC449")
-    print( " n = 3 ")
-    resp = view_by_community("CSUF-CPSC449","3")
-    assert resp.status_code == 200, 'FAIL - Expected status code 200. Got status code' + str(resp.status_code)
-    #assert len(resp.json()['data']) == 3 , "FAIL - Expected the length of the json to be 3. Instead received" + str(len(resp.json()["data"]))
-    print("PASS - view all last 3 posts returned 3 posts")
-    if(args.verbose):
-        print(resp.json()['data'])
-
     print( " n = 5 ")
     resp = view_by_community("CSUF-CPSC449","5")
     assert resp.status_code == 200, 'FAIL - Expected status code 200. Got status code' + str(resp.status_code)
@@ -121,6 +129,8 @@ def main():
     print("PASS - view all last 5 posts returned 5 posts")
     if(args.verbose):
         print(resp.json()['data'])
+
+    """
 
 
 if __name__=="__main__":
